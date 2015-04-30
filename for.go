efd18d7083e8f529726dcabb5c98facacc1e3cd1
Ankit gupta <ankit.gupta8898@gmail.com>
2015-04-30 11:13:03 -0400

    go try's

---
+++ for.go
@@ -0,0 +1,36 @@
+// `for` is Go's only looping construct. Here are
+// three basic types of `for` loops.
+
+package main
+
+import "fmt"
+
+func main() {
+
+
+    i := 1
+    for i <= 4 {
+        fmt.Println(i);
+        i++;
+    }
+
+    // // The most basic type, with a single condition.
+    // i := 1
+    // for i <= 3 {
+    //     fmt.Println(i)
+    //     i = i + 1
+    // }
+
+    // // A classic initial/condition/after `for` loop.
+    // for j := 7; j <= 9; j++ {
+    //     fmt.Println(j)
+    // }
+
+    // // `for` without a condition will loop repeatedly
+    // // until you `break` out of the loop or `return` from
+    // // the enclosing function.
+    // for {
+    //     fmt.Println("loop")
+    //     break
+    // }
+}
---
+++ variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+   var name string = "Ankit";
+
+   fmt.Println(name);
+
+   var a,b int = 3,4;
+
+   //sum
+   fmt.Println("Sum",a+b);
+
+   var bool = true;
+
+   fmt.Println("Boolean",bool);
+
+
+   var t float32 = 3.2;
+
+   fmt.Println("Float -",t);
+
+}
